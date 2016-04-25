380b94ee689f3eb6c7f8d1f6b5d1c3caf60940b7
Ross Drew <ross.drew@evogi.com>
2016-04-25 17:54:12 +0100

    Endpoint simulator platform and test endpoint package

---
+++ EndpointSimulator.py
@@ -0,0 +1,49 @@
+import web
+import sys
+import importlib
+
+programID = "Endpoint Simulator v0.1"
+urls = ('/(.*)', 'base')
+
+def importAllEndpointFiles(endpointDefinitions, urls):
+	""" Import all python files from endpointDefinitions and 
+	    add their urls to the presented ones.
+
+		>>> importAllEndpointFiles (['Test'])
+		('/(.*)', 'base', '/Test/(.*)', 'test')"""
+
+	completeUrlList = ()
+	for enpointFile in endpointDefinitions:
+		print "Attempting to import {}".format(enpointFile)
+		try:
+			importlib.import_module(enpointFile)
+			urlList = sys.modules[enpointFile].urls
+			completeUrlList += urlList
+			#command_module.run()  ##What's this for?
+			print "Imported {}.py presenting the endpoints {}".format(enpointFile, urlList)
+		except ImportError:
+			print "Cannot import {}".format(enpointFile)
+
+	completeUrlList += urls
+	return completeUrlList
+
+#TODO Need to order these 
+#So I'll want to be able to add to this and have them ordered by granualty, most fine-grained first
+allUrls = importAllEndpointFiles(sys.argv[2:], urls)
+print "Offering : \n{}".format(allUrls)
+
+#Classes in 'urls' inside packages need to be prefixed with the package name, e.g. 'Test.test'
+app = web.application(allUrls, locals())
+
+
+
+################################
+#  Endpoint Definition Classes #
+################################
+
+class base:        
+    def GET(self, name):
+        return programID
+
+if __name__ == "__main__":
+    app.run()
---
+++ Test.py
@@ -0,0 +1,12 @@
+
+print "Test imported..."
+
+urls = ('/Test', 'Test.test')
+
+def getURLS():
+	return urls
+
+class test:        
+    def GET(self):
+    	print "TEST HIT"
+        return "Test Endpoint!"
